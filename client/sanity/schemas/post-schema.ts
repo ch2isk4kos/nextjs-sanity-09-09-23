cd53931ccfebf87b264172b0b08e357842c90ad0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-09 19:16:24 -0400

    add description object to fields array'

--- client/sanity/schemas/post-schema.ts
+++ client/sanity/schemas/post-schema.ts
@@ -4,10 +4,11 @@ const post = {
   type: "document",
   fields: [
     { name: "title", title: "Title", type: "string"},
+    { name: "description", title: "Description", type: "string"},
     { name: "slug", title: "Slug", type: "slug", options: { source: "name" }},
     { name: "image", title: "Image", type: "image", options: { hotspot: true }, fields: [{ name: "alt", title: "Alt", type: "string" }]},
     { name: "url", title: "URL", type: "url"},
-    { name: "content", title: "Content", type: "array", of: { type: "block" }},
+    { name: "content", title: "Content", type: "array", of: [{ type: "block" }]},
   ]
 }
 
