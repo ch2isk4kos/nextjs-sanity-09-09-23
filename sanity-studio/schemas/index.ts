aa84bcdf69ff9d7b6603d954809bed6cff0edd1a
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-09 18:22:17 -0400

    initialize sanity studio

---
+++ sanity-studio/schemas/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Sanity data lives in a cloud referred to as "The Content Lake" within datasets.
+ * A dataset is a collection of json documents of different types and references.
+ * When building schemas - you'll be able to to see the raw objects of the data.
+ */
+
+export const schemaTypes = []
