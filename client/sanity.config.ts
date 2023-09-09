d200b60edfa24e2b9b57795724fe4d489d0a6321
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-09 19:17:53 -0400

    add schema object key/value to sanity configuration

--- client/sanity.config.ts
+++ client/sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import post from "./sanity/schemas/post-schema";
 
 const config = defineConfig({
   projectId: "p0m10kju",
@@ -7,7 +8,8 @@ const config = defineConfig({
   title: "Next.js with Sanity Studio Demo",
   apiVersion: "2023-09-09",
   basePath: "/admin",
-  plugins: [deskTool()]
+  plugins: [deskTool()],
+  schema: { types: [ post ]},
 })
 
 export default config;
