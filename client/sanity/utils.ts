af7d6ef96756f05d38518a76e66bcf3ee847ab22
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-09 19:44:40 -0400

    wrote a utility function to get posts from sanity's content data lake

---
+++ client/sanity/utils.ts
@@ -0,0 +1,21 @@
+import { createClient, groq } from "next-sanity";
+
+export async function getPosts() {
+  const client = createClient({
+    projectId: "p0m10kju",
+    dataset: "production",
+    apiVersion: "2023-09-09",
+  });
+
+  return client.fetch(
+    groq`*[_type = "post"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`
+  )
+}
