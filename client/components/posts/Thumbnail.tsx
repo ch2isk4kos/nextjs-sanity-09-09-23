99e9ddf0a8e5943f755a1a3d364af1bd7a7f325a
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-10 14:00:55 -0400

    test root tailwind variable

--- client/components/posts/Thumbnail.tsx
+++ client/components/posts/Thumbnail.tsx
@@ -1,9 +1,14 @@
+// const root = `mx-auto my-5 bg-blue-400`;
+// const root = `py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6`;
+const root = ``;
+
+
 function Thumbnail({ ...props }) {
+  const {title} = props;
+  
   return (
-    <div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
+    <div className={root}>
+      <h3>{title}</h3>
     </div>
   )
 }
