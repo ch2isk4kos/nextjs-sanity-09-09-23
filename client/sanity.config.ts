dfc53196743605f3802e610920e810396b4a99b0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-09 20:01:58 -0400

    import schemas array and pass to sanity configuration object

--- client/sanity.config.ts
+++ client/sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-import post from "./sanity/schemas/post-schema";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "p0m10kju",
@@ -9,7 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-09-09",
   basePath: "/admin",
   plugins: [deskTool()],
-  schema: { types: [ post ]},
+  schema: { types: schemas},
 })
 
 export default config;
