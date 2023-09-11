c78c4be1342381e7acba00ecf6ef0df441a47189
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-11 14:08:17 -0400

    abstracted util functionality

--- client/sanity/utils.ts
+++ client/sanity/utils.ts
@@ -1,14 +1,14 @@
 import { createClient, groq } from "next-sanity";
 import { Post } from "@/ts-types/Post";
 
+const client = createClient({
+  projectId: "p0m10kju",
+  dataset: "production",
+  apiVersion: "2023-09-09",
+});
+
 // anywhere that uses this utility function will return a Promise with an array of Post objects
 export async function getPosts(): Promise<Post[]> {
-  const client = createClient({
-    projectId: "p0m10kju",
-    dataset: "production",
-    apiVersion: "2023-09-09",
-  });
-
   return client.fetch(
     groq`*[_type == "post"]{
       _id,
@@ -21,3 +21,19 @@ export async function getPosts(): Promise<Post[]> {
     }`
   )
 }
+
+// returns 3 most recent posts
+export async function getRecentPosts(): Promise<Post[]> {
+  return client.fetch(
+    groq`*[_type == "post"] | order(_createdAt desc) [0..2]{
+      _id,
+      _createdAt,
+      title,
+      description,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url
+    }`
+  )
+}
+
