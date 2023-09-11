7bc9d5620cde32d8a497b970cfa871ed2ca854cf
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-11 14:19:55 -0400

    add/remove comment(s)

--- client/app/posts/page.tsx
+++ client/app/posts/page.tsx
@@ -1,24 +1,22 @@
 import Thumbnail from "@/components/posts/Thumbnail";
-import { getPosts } from "@/sanity/utils";
+import { getPosts, getRecentPosts } from "@/sanity/utils";
 
 // TailwindCSS variables
 const root = `max-w-5xl mx-auto my-[5%] py-50`
 const h1 = `text-7xl font-extrabold`
-const h2 = `my-5 text-3xl font-extrabold`
 const span = `bg-gradient-to-r from-blue-800 via-blue-600 to-blue-800 bg-clip-text text-transparent`
 const header = `grid grid-cols-3 gap-3 border-4 border-red-400`
+const section = ``;
 
 async function PostsPage() {
-  const posts = await getPosts();
+  const recent = await getRecentPosts();
+  const all = await getPosts();
   return (
     <div className={root}>
       <h1 className={h1}><span className={span}>Posts</span></h1>
-      {/* <h2 className={h2}>Highlights</h2> */}
+      {/* Header */}
       <header className={header}>
-        {posts.map((post) => (
-          // <div key={post._id}>
-          //   {post.title}
-          // </div>
+        {recent.map((post) => (
           <Thumbnail 
             key={post._id}
             date={post._createdAt}
@@ -28,6 +26,18 @@ async function PostsPage() {
           />
         ))}
       </header>
+      {/* Section 1 */}
+      <section className={section}>
+        {all.map((post) => (
+          <Thumbnail 
+            key={post._id}
+            date={post._createdAt}
+            description={post.description}
+            image={post.image}
+            title={post.title}
+          />
+        ))}
+      </section>
     </div>
   );
 }
