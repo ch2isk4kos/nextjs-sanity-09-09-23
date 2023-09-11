aa1ee0cb76c3eb5871f22ef4e09d9399bd20c581
ch2isk4kos <ch2isk4kos@gmail.com>
2023-09-11 13:32:19 -0400

    pass in/render props

--- client/components/posts/Thumbnail.tsx
+++ client/components/posts/Thumbnail.tsx
@@ -1,14 +1,22 @@
 // const root = `mx-auto my-5 bg-blue-400`;
 // const root = `py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6`;
-const root = ``;
-
+const root = `flex flex-col items-center w-auto h-[25rem] bg-blue-500`;
+const img = `h-[60%] w-[100%] bg-white`;
+const h3 = `text-xl`;
+const pDate = `display-0 text-xs`;
+const pDescription = ``;
 
 function Thumbnail({ ...props }) {
-  const {title} = props;
-  
+  // console.log(Date(props.date));
+  const {date, description, image, title} = props;
+  const frmt = new Date(date);
+
   return (
     <div className={root}>
-      <h3>{title}</h3>
+      <img className={img} src={image}></img>
+      <h3 className={h3}>{title}</h3>
+      <p className={pDate}>{frmt.toDateString()}</p>
+      <p className={pDescription}>{description}</p>
     </div>
   )
 }
--- client/ts-types/Post.ts
+++ client/ts-types/Post.ts
@@ -4,6 +4,7 @@ export type Post = {
   _id: string;
   _createdAt: Date;
   title: string;
+  description: string;
   slug: string;
   image: string;
   url: string;
